4dae519353d3689b40b9a0f1e83da9860f5882f7
seatwork <app@zerg.cc>
2023-02-17 11:13:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,15 @@ const Cross = require("node-cross");
 const bark = require("./bark.js");
 
 new Cross()
-  .use(async (ctx, next) => {
-    ctx.set("Access-Control-Allow-Credentials", true);
-    ctx.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    ctx.set("Access-Control-Allow-Headers", "Origin,Content-Type,Accept");
-    ctx.set("Access-Control-Allow-Origin", "https://zerg.cc");
-    await next();
-  })
-  .get("/", () => {
-    return "Node.js implementation of the [bark-server](https://github.com/Finb/bark-server).";
-  })
-  .get("/ping", bark.ping)
-  .get("/register", bark.register)
-  .post("/:key", bark.push)
-  .listen();
+.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Credentials", true);
+  ctx.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  ctx.set("Access-Control-Allow-Headers", "Origin,Content-Type,Accept");
+  ctx.set("Access-Control-Allow-Origin", "*");
+  await next();
+})
+.get("/", () => "https://github.com/metadream/bark-server")
+.get("/ping", bark.ping)
+.get("/register", bark.register)
+.post("/:key", bark.push)
+.listen();
